e32975b06c464aabc076fdb386e78c80ae578b27
Gergely2004 <techmanology24@gmail.com>
2025-06-08 16:38:13 +0300

    kepek
    kepek is ok

--- my-blog/pages/Media.js
+++ my-blog/pages/Media.js
@@ -1,7 +1,12 @@
-import aiImage from "../images/ai.jpg";
-import healthyImage from "../images/healthy.jpeg";
-import programmingImage from "../images/programming.png";
-import Header from "../components/Header"
+// import aiImage from "../images/ai.jpg";
+// import healthyImage from "../images/healthy.jpeg";
+// import programmingImage from "../images/programming.png";
+// import Header from "../components/Header"
+import aiImage from "../public/ai.jpg"
+import healthyImage from "../public/healthy.jpeg";
+import programmingImage from "../public/programming.png";
+
+import Header from '../components/Header'
 
 
 export default function Media() {
@@ -11,9 +16,9 @@ export default function Media() {
             <main className="media">
                 <h2>Media</h2>
                 <div className="gallery">
-                    <img src={aiImage} alt="Kep1" />
-                    <img src={healthyImage} alt="Kep2" />
-                    <img src={programmingImage} alt="Kep3" />
+                    <img src="/ai.jpg" alt="Kep1" />
+                    <img src="/healthy.jpeg" alt="Kep2" />
+                    <img src="/programming.png" alt="Kep3" />
                     <iframe id="video"
                         width="560" height="315"
                         src="https://www.youtube.com/embed/dQw4w9WgXcQ"
